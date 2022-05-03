f530a4b6b959d220b4196d8a248da728654f68a2
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-05-03 12:52:56 +0800

    Add description, author, keywords meta tags

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -5,6 +5,7 @@ interface Props {
   description?: string;
   imageUrl?: string;
   type?: string;
+  keywords: string[];
 }
 
 function PageHead(props: Props) {
@@ -16,11 +17,20 @@ function PageHead(props: Props) {
       property="og:title"
       content={props?.title || 'Blog | Yuanlin Lin 林沅霖'}
       key="title" />
+    <meta
+      name="description"
+      content={props?.description || '我是林沅霖，目前就讀於浙江大學資訊工程系。' +
+        '我熱愛產品設計與軟體開發，擅長分析複雜的問題並提供有效的解決方案。歡迎查看我的部落格！'
+      } />
+    <meta
+      name="keywords"
+      content={props.keywords?.join(', ') || 'React, Javascript, Typescript, 網站開發, 軟體開發, 前端, 後端, UI, UX, Go, Golang, Python, Django, Blog, 部落格, Yuanlin, 林沅霖, html, css'} />
+    <meta name="author" content="Yuanlin Lin 林沅霖" />
     <meta property="og:type" content={props.type || 'website'} key="type" />
     <meta
       property="og:description"
       content={props?.description || '我是林沅霖，目前就讀於浙江大學資訊工程系。' +
-        '我熱愛產品設計與軟體開發，擅長分析複雜的問題並提供有效的解決方案。歡迎查看我的作品集與部落格！'
+        '我熱愛產品設計與軟體開發，擅長分析複雜的問題並提供有效的解決方案。歡迎查看我的部落格！'
       }
       key="description" />
     <meta
