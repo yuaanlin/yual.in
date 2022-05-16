e43066088fa78ba5646aaebc374130b0b355289f
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-05-16 14:09:09 +0800

    Fix image display

--- components/FadeInImage.tsx
+++ components/FadeInImage.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import cx from 'classnames';
-
 interface Props {
   className?: string;
   src?: string;
@@ -8,34 +5,13 @@ interface Props {
 }
 
 function fadeInImage(props: Props) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [isSafari, setIsSafari] = useState(false);
-
-  useEffect(() => {
-    setIsLoaded(false);
-  }, [props.src]);
-
-  useEffect(() => {
-    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
-  }, []);
-
-  if (isSafari) {
-    return (
-      <img
-        className={props.className}
-        src={props.src}
-        alt={props.alt}
-      />
-    );
-  }
-
-  return <img
-    onLoad={() => setIsLoaded(true)}
-    className={cx(props.className,
-      isLoaded ? 'opacity-100' : 'opacity-0', 'transition')}
-    src={props.src}
-    alt={props.alt}
-  />;
+  return (
+    <img
+      className={props.className}
+      src={props.src}
+      alt={props.alt}
+    />
+  );
 }
 
 export default fadeInImage;
